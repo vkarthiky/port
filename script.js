db1676be137d7cbdcf493266035c3885de419e08
karthik <karthikyv157@gmail.com>
2024-07-29 11:45:18 -0700

    swipe gesture

--- script.js
+++ script.js
@@ -2,11 +2,3 @@ function showMenu() {
     document.getElementById('iphone-home').style.display = 'none';
     document.getElementById('menu').style.display = 'flex';
 }
-
-function showNextExperience(experienceId) {
-    const experiences = document.querySelectorAll('.experience-card');
-    experiences.forEach(exp => {
-        exp.style.display = 'none';
-    });
-    document.getElementById(experienceId).style.display = 'flex';
-}
